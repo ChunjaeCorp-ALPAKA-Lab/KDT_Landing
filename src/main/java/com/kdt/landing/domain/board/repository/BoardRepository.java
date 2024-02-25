c836768d356f6a86fe1e1ca9a38d7116e1ac4bcf
RosieOh <dhxogns920@gmail.com>
2024-02-25 17:56:21 +0900

    🏫feat : board에 필요한 JPA 서비스 로직 구현 #13🏫

---
+++ src/main/java/com/kdt/landing/domain/board/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.kdt.landing.domain.board.repository;
+
+import com.kdt.landing.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
