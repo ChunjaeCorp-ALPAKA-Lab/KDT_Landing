4fe023c8e7cff2209d49865afb38e0a40989e76e
RosieOh <dhxogns920@gmail.com>
2024-02-26 13:41:51 +0900

    🏫feat : Board 객체를 받아줄 BoardDTO 제작 완 #13🏫

---
+++ src/main/java/com/kdt/landing/domain/board/service/BoardServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kdt.landing.domain.board.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+}
